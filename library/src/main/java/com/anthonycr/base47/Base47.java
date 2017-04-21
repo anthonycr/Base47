1b769f3169e4fa388318463177475a288d19f6b9
anthony restaino <anthonyrestaino11@gmail.com>
2017-04-20 21:29:59 -0400

    Removing unnecessary characters from binary array

--- library/src/main/java/com/anthonycr/base47/Base47.java
+++ library/src/main/java/com/anthonycr/base47/Base47.java
@@ -52,10 +52,7 @@ public final class Base47 {
     private static final int BASE_47 = 47;
 
     private static final String[] CHARACTERS_2_ARRAY = {
-        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I",
-        "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b",
-        "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
-        "v", "w", "x", "y", "z"
+        "0", "1"
     };
 
     private static final String[] CHARACTERS_ARRAY = {
@@ -186,10 +183,10 @@ public final class Base47 {
             newNumber.insert(0, newDigit);
         }
 
-
         if (newNumber.length() == 0) {
             return zeroAsBase(newBase);
         }
+
         return newNumber.toString();
     }
 
